5cec5fae9a02cbbcb6528bd76c7e2c7a7ea035c9
Dovi-Amiram <73857923+Dovi-Amiram@users.noreply.github.com>
2023-03-13 18:34:26 +0200

    Add files via upload
    commit

---
+++ Client.py
@@ -0,0 +1,293 @@
+# Importing necessary modules for packet manipulation
+from scapy.all import *
+# Importing modules for handling DHCP packets
+from scapy.layers.dhcp import DHCP, BOOTP
+# Importing modules for handling IP and UDP protocols
+from scapy.layers.inet import IP, UDP
+# Importing module for handling Ethernet frames
+from scapy.layers.l2 import Ether
+# imports the socket module which provides a low-level networking interface
+import random
+import struct
+import socket as s
+import time
+
+"""
+*************************************************************
+                        DHCP client
+**************************************************************
+"""
+
+
+class DHCPClient:
+    def __init__(self):
+        # Get the MAC address of the interface being used
+        self.mac_address = get_if_hwaddr(conf.iface)
+        # Initialize a variable to hold the IP address assigned by DHCP (not yet known)
+        self.ip_address = None
+        self.DNSserver_ip = None
+
+    def handle_offer_packet(self, packet):
+        # Print a message indicating that an offer packet was received
+        print("DHCP offer received")
+        # Send a DHCP request packet in response to the offer
+        self.send_request_packet(packet)
+
+    def handle_ack_packet(self, ack):
+        print("DHCP ack received")
+        self.DNSserver_ip = ack[DHCP].options[5][1]
+
+    def send_discover_packet(self):
+        # Construct a DHCP Discover packet using the current MAC address as the client identifier
+        discover = Ether(src=self.mac_address, dst='ff:ff:ff:ff:ff:ff') / \
+                   IP(src='0.0.0.0', dst='255.255.255.255') / \
+                   UDP(sport=68, dport=67) / \
+                   BOOTP(op=1, chaddr=self.mac_address) / \
+                   DHCP(options=[('message-type', 'discover'), 'end', 'pad'])
+        # Print a message indicating that a Discover packet is being sent
+        print("sending DHCP Discover")
+        # Send the Discover packet over the network
+        sendp(discover)
+        # Sniff the network for incoming DHCP offer packets and call handle_offer_packet() when one is received
+        sniff(prn=self.handle_offer_packet, filter='(port 67 or port 68)', count=1)
+
+    def send_request_packet(self, offer):
+        # Extract the offered IP address from the offer packet and construct a DHCP Request packet
+        ip_address = offer[BOOTP].yiaddr
+        request = Ether(src=self.mac_address, dst='ff:ff:ff:ff:ff:ff') / \
+                  IP(src='0.0.0.0', dst='255.255.255.255') / \
+                  UDP(sport=68, dport=67) / \
+                  BOOTP(op=1, chaddr=self.mac_address, yiaddr=ip_address, siaddr=get_if_addr(conf.iface)) / \
+                  DHCP(options=[('message-type', 'request'),
+                                ('requested_addr', ip_address),
+                                ('server_id', offer[DHCP].options[1][1]), 'end', 'pad'])
+        # Wait for 1 second before sending the request packet
+        time.sleep(1)
+        # Print a message indicating that a Request packet is being sent
+        print("sending DHCP Request")
+        # Send the Request packet over the network
+        sendp(request)
+        # Sniff the network for incoming DHCP ACK packets and call handle_ack_packet() when one is received
+        sniff(prn=self.handle_ack_packet, filter='(port 67 or port 68)', count=1)
+
+
+"""
+*************************************************************
+                        DNS client
+**************************************************************
+"""
+
+import socket
+
+
+class DNSClient:
+    def _init_(self):
+        # Set the IP address and port number for the DNS server
+        self.server_address = ('0.0.0.0', 53)
+        self.domain_ip = None
+
+    def query(self, domain):
+        # create a UDP socket
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # set the DNS server address and port
+
+        # construct the DNS query packet
+        packet = self.create_dns_packet(domain)
+
+        # send the packet to the DNS server
+        client_socket.sendto(packet, self.server_address)
+
+        # receive the response from the DNS server
+        response, server_address = client_socket.recvfrom(4096)
+
+        # parse the DNS response packet
+        header = response[:12]
+        answer_section = response[12 + len(packet):]
+
+        # extract the IP address from the answer section
+        ip_bytes = answer_section[-4:]
+        self.domain_ip = socket.inet_ntoa(ip_bytes)
+
+        # close the socket
+        client_socket.close()
+
+        return response, self.domain_ip
+
+    def create_dns_packet(self, domain):
+        # construct the DNS query packet
+        packet = b''
+        packet += b'\x00\x01'  # ID
+        packet += b'\x01\x00'  # QR=0, Opcode=0, AA=0, TC=0, RD=1
+        packet += b'\x00\x01'  # QDCOUNT (1 question)
+        packet += b'\x00\x00'  # ANCOUNT
+        packet += b'\x00\x00'  # NSCOUNT
+        packet += b'\x00\x00'  # ARCOUNT
+        # construct the question section
+        parts = domain.split('.')
+        for part in parts:
+            packet += bytes([len(part)])
+            packet += part.encode('ascii')
+        packet += b'\x00'  # end of domain name
+        packet += b'\x00\x01'  # QTYPE=A
+        packet += b'\x00\x01'  # QCLASS=IN
+
+        return packet
+
+
+
+"""
+    *************************************************************
+                           RUDP client
+    *************************************************************
+"""
+
+# Constants
+CHUNK = 2048  # Maximum data size in packet
+HEADER_SIZE = 8  # Size of packet header
+ATTEMPT_LIMIT = 10
+DATA_PACKET = 0  # Packet type for data packets
+ACK_PACKET = 1  # Packet type for acknowledgement packets
+SYN_PACKET = 2  # Packet type for syn packet
+SYN_ACK_PACKET = 3  # Packet type for syn ack packet
+FILE_SIZE_INFO = 4  # a special packet type for indicating the file size to be sent.
+CLOSE_CONNECTION = 5  # Packet type for closing connection
+CLOSE_CONNECTION_ACK = 6  # Packet type for acknowledging connection closure
+REQUEST_PACKET = 7
+REQUEST_ACK = 8
+FORMAT = '!II'  # format string for struct.pack and struct.unpack
+TIMEOUT = 2
+
+
+def deconstruct_packet(packet):
+    seq, packet_type = struct.unpack(FORMAT, packet[0][:HEADER_SIZE])
+    return {'type': packet_type, 'seq': seq, 'src_address': packet[1], 'data': packet[0][HEADER_SIZE:]}
+
+
+class RUDPClient:
+
+    def __init__(self):
+        """
+        Constructor for the ReliableUDP class
+        :param host_adress: port and ip
+        :param server: boolean flag indicating whether this instance is running as a server or client
+        """
+        self.sock = s.socket(s.AF_INET, s.SOCK_DGRAM)  # Create a UDP socket
+        self.sock.setblocking(False)  # Set socket to non-blocking mode
+        self.sock.settimeout(TIMEOUT)  # Set socket timeout to 1 second
+        self.received_packets = {}  # Dictionary for holding received data payloads
+        self.outgoing_seq = random.randint(0, (2 ** 16))  # Sequence number for outgoing packets
+        self.server_address = None  # tuple: (ip, port)
+        self.all_data_received = False
+        self.request_accepted = False
+
+    def connect(self, server_ip, server_port):
+        for i in range(ATTEMPT_LIMIT):
+            # create a SYN packet
+            syn_packet = struct.pack(FORMAT, self.outgoing_seq, SYN_PACKET)
+            # send the SYN packet to the server
+            self.sock.sendto(syn_packet, (server_ip, server_port))
+
+            time.sleep(2)  # 2 seconds grace period
+
+            # receive syn ack
+            try:
+                # wait for SYN-ACK response from server
+                type, seq, address, data = deconstruct_packet(self.sock.recvfrom(CHUNK)).values()
+                # verify that the packet is a SYN-ACK packet
+                if type == SYN_ACK_PACKET:
+                    # send ACK packet to server
+                    print("Connection with server established...")
+                    return True
+            except socket.timeout:
+                self.outgoing_seq += 1
+        return False
+
+    def send_request(self, request):
+        http_request_packet = struct.pack(FORMAT, self.outgoing_seq, REQUEST_PACKET)
+        self.outgoing_seq += 1
+        http_request_packet += request
+        self.sock.sendto(http_request_packet, self.server_address)
+        self.receive_data()
+
+    def receive_data(self):
+        packets_to_be_received = float('inf')
+        while not self.all_data_received:
+            try:
+                type, seq, address, data = deconstruct_packet(self.sock.recvfrom(CHUNK)).values()
+                if type == DATA_PACKET:
+                    self.received_packets[seq] = {'src address': address, 'data': data}
+                    packets_to_be_received -= 1
+                    self.ack(seq)
+                elif type == FILE_SIZE_INFO:
+                    packets_to_be_received = int(data.decode()) - len(self.received_packets)
+                    self.ack(seq)
+                else: # type == REQUEST_ACK:
+                    self.request_accepted = True
+            except socket.timeout:
+                if packets_to_be_received == 0:
+                    self.all_data_received = True
+                continue
+
+    def ack(self, seq):
+        """
+        Sends an acknowledgement packet for a specified sequence number to the src address
+        :param seq: the sequence number of the packet to acknowledge
+        """
+        ack = struct.pack(FORMAT, seq, ACK_PACKET)
+        self.sock.sendto(ack, self.received_packets[seq]['src address'])
+
+
+def client_request(url, file_name):
+    """
+    *************************************************************
+                           DHCP request
+    **************************************************************
+    """
+
+    # Create a DHCPClient object
+    dhcp_client = DHCPClient()
+
+    # Call the send_discover_packet() function to initiate the DHCP process
+    dhcp_client.send_discover_packet()
+
+    dns_ip = dhcp_client.DNSserver_ip
+
+    """
+    *************************************************************
+                    DNS query
+    **************************************************************
+    """
+
+    # Create a DNSClient object
+    dns_client = DNSClient()
+
+    # Query the DNS server for the IP address of downloadmanager.com
+    _, app_server_ip = dns_client.query("downloadmanager.com")
+
+
+    """
+    *************************************************************
+                        HTTP request
+    **************************************************************
+    """
+
+    http_request = f"GET /{file_name} HTTP/1.1\r\nHost: {url}\r\n\r\n".encode()
+    rudp_c = RUDPClient()
+    rudp_c.connect(app_server_ip, 50000)
+    rudp_c.send_request(http_request)
+    rudp_c.receive_data()
+    if rudp_c.all_data_received:
+        data = b''
+        packets = sorted(rudp_c.received_packets.items(), key=lambda item:item[0])
+        for i in range(len(packets)):
+            data += packets[i][1]['data']
+        output = data.decode('utf-8')
+        with open(file_name, 'w') as f:
+            f.write(output)
+    else:
+        print("something went wrong")
+
+
+if __name__ == '__main__':
+    client_request("", "")
---
+++ Server.py
@@ -0,0 +1,225 @@
+import random
+import socket
+import struct
+import socket as s
+import time
+import requests
+
+# Constants
+CHUNK = 2048  # Maximum data size in packet
+HEADER_SIZE = 8  # Size of packet header
+DATA_PACKET = 0  # Packet type for data packets
+ACK_PACKET = 1  # Packet type for acknowledgement packets
+SYN_PACKET = 2  # Packet type for syn packet
+SYN_ACK_PACKET = 3  # Packet type for syn ack packet
+FILE_SIZE_INFO = 4  # a special packet type for indicating the file size to be sent.
+CLOSE_CONNECTION = 5  # Packet type for closing connection
+CLOSE_CONNECTION_ACK = 6  # Packet type for acknowledging connection closure #TODO ack
+REQUEST_PACKET = 7
+REQUEST_ACK = 8
+FORMAT = '!II'  # format string for struct.pack and struct.unpack
+TIMEOUT = 2
+ATTEMPT_LIMIT = 10
+
+# conventional constants for cubic CC algo:
+C = 0.4
+beta = 0.7
+
+
+def deconstruct_packet(packet):
+    seq, packet_type = struct.unpack(FORMAT, packet[0][:HEADER_SIZE])
+    return {'type': packet_type, 'seq': seq, 'src_address': packet[1], 'data': packet[0][HEADER_SIZE:]}
+
+
+class RUDPServer:
+    def __init__(self, ip, port):
+        self.sock = s.socket(s.AF_INET, s.SOCK_DGRAM)  # Create a UDP socket
+        self.sock.setblocking(False)  # Set socket to non-blocking mode
+        self.sock.settimeout(TIMEOUT)  # Set socket timeout to 1 second
+        self.packets_to_send = {}  # Dictionary for holding packets to be sent
+        self.sent_items = {}  # Dictionary for holding packets that have been sent but not yet acknowledged
+        self.outgoing_seq = random.randint(0, (2 ** 16))  # Sequence number for outgoing packets
+        self.cwnd = 1  # Initial congestion window size
+        self.w_max = self.cwnd  # Maximum window size
+        self.slow_start_threshold = 16  # Initial slow start threshold
+        self.congestion_avoidance = False  # Flag for indicating whether we are currently in congestion avoidance phase
+        self.last_window_reduction = None  # Timestamp for the last time we reduced the congestion window
+        self.file_size = 0  # Size of the file to be sent
+        self.rtt = 0  # Estimated round-trip time in seconds
+        self.server_address = ip, port
+        self.connected = False
+        self.requests = {}
+
+    # server functions
+
+    def bind(self):
+        """
+        Bind the socket to the specified address and port.
+        """
+        self.sock.bind(self.server_address)
+
+    def accept_connection(self):
+        while not self.connected:
+            try:
+                type, seq, address, _ = deconstruct_packet(self.sock.recvfrom(CHUNK)).values()
+                if type == SYN_PACKET:
+                    syn_ack_packet = struct.pack(FORMAT, self.outgoing_seq, SYN_ACK_PACKET)
+                    self.sock.sendto(syn_ack_packet, address)
+                    self.connected = True
+                    print("ready to serve...")
+            except socket.timeout:
+                continue
+
+    def receive_packet(self):
+        type, seq, address, data = deconstruct_packet(self.sock.recvfrom(CHUNK)).values()
+        return type, seq, address, data
+
+    def cubic_algo(self, T):
+        """
+        Implementation of the Cubic Congestion Control algorithm.
+        :param T: Time elapsed since last window reduction
+        :return: New congestion window size
+        """
+        K = ((self.w_max * (1 - beta)) / C) ** (1 / 3)
+        cwnd = C * ((T - K) ** 3) + self.w_max
+        return cwnd
+
+    def send_data(self, address):
+        while True:
+            # slow start
+            if not self.congestion_avoidance:
+                if self.cwnd >= self.slow_start_threshold:
+                    self.congestion_avoidance = True
+                else:
+                    self.cwnd *= 2
+            # congestion avoidance
+            else:
+                now = time.time()
+                time_elapsed = now - self.last_window_reduction
+                # calculate new congestion window size using cubic algorithm
+                new_cwnd = self.cubic_algo(time_elapsed)
+
+                if new_cwnd >= self.cwnd:
+                    self.cwnd = new_cwnd
+                else:  # the new window is smaller than the old window
+                    self.w_max = self.cwnd
+                    self.last_window_reduction = time.time()
+                    self.slow_start_threshold = max(self.cwnd / 2, 1)
+                    self.cwnd = self.slow_start_threshold
+                    self.congestion_avoidance = False
+
+            # send payload
+            packets = []
+            first_seq_sent = self.outgoing_seq
+            for i in range(self.cwnd):
+                packets.append(self.packets_to_send[i])
+                self.sent_items[i] = self.packets_to_send[i]
+                self.outgoing_seq += 1
+
+            # Send all the packets at once using the socket
+            time_of_sending = time.time()
+            self.sock.sendto(b''.join(packets), address)
+
+            # receive acks
+            try:
+                while True:
+                    type, seq, _, _ = deconstruct_packet(self.sock.recvfrom(CHUNK)).values()
+                    time_of_ack = time.time()
+
+                    if type == ACK_PACKET and seq in self.sent_items:  # if this is an acknowledgement packet for a
+                        # sent packet
+                        if seq == first_seq_sent:
+                            self.rtt = time_of_sending - time_of_ack
+                            self.sock.settimeout(self.rtt / 2)
+                        self.sent_items.pop(seq)
+            except socket.timeout:
+                if len(self.packets_to_send) == len(self.sent_items) == 0:  # all packets were sent and acked
+                    break
+
+        # loop breaks if and only if all packets were sent and acked, and only then connection is closed:
+        self.close_connection()
+
+    def construct_payload(self, data):
+        packet_count_info = struct.pack(FORMAT, self.outgoing_seq, FILE_SIZE_INFO)
+        packet_count_info += str(len(self.packets_to_send)).encode()
+        self.packets_to_send[self.outgoing_seq] = packet_count_info
+        self.outgoing_seq += 1
+        for i in range((self.file_size // CHUNK) + 1):
+            data_packet = struct.pack(FORMAT, self.outgoing_seq, DATA_PACKET)
+            data_packet += data[i * CHUNK:(i + 1) * CHUNK]
+            self.packets_to_send[self.outgoing_seq] = data_packet
+            self.outgoing_seq += 1
+
+    def close_connection(self):
+
+        # This function is responsible for closing the connection after all data has been sent and acknowledged. It
+        # sends a CLOSE_CONNECTION packet to the other end and waits for a CLOSE_CONNECTION_ACK packet before closing
+        # the socket.
+
+        while True:
+            # construct the CLOSE_CONNECTION packet and send it
+            packet = struct.pack(FORMAT, self.outgoing_seq, CLOSE_CONNECTION)
+            packet += b'Closing connection'
+            self.sock.send(packet)
+            try:
+                # wait for a response packet from the other end
+                type, seq, _, _ = deconstruct_packet(self.sock.recvfrom(CHUNK)).values()
+
+                if type == CLOSE_CONNECTION_ACK:
+                    # if a CLOSE_CONNECTION_ACK packet is received, close the socket and exit the loop
+                    self.sock.close()
+                    print("Connection closed.")
+                    break
+            except socket.timeout:
+                # if an error occurs while waiting for the response packet, continue waiting
+                continue
+
+
+# app server
+
+IP = "127.0.0.1"
+PORT = 30000
+
+
+def downloadmanager():
+    rudp_s = RUDPServer(IP, PORT)
+    rudp_s.bind()
+    print("Ready to serve...")
+
+    time_to_wait = 60
+    elapsed_time = 0
+    current_time = time.time()
+    time_stamp = current_time
+    request = ''
+    while time_to_wait > 0:
+        try:
+            t, seq, address, data = rudp_s.receive_packet()
+
+            print("Request received")
+            # string manipulation to extract host name anf file name
+            request_string = data.decode('utf-8')
+            request_lines = request_string.split("\r\n")
+            file_name = request_lines[0][5: -9]
+            host = request_lines[1][6:]
+            url = f'http://{host}/{file_name}'
+
+            # http get request
+            response = requests.get(url)
+            data = response.content
+            rudp_s.construct_payload(data)
+            rudp_s.send_data(address)
+            print("file sent.")
+
+        except socket.timeout:
+            pass
+        time_stamp = time.time()
+        elapsed_time = current_time - time_stamp
+        current_time = time_stamp
+        time_to_wait -= elapsed_time
+    print("No requests submitted for more than a minute! closing connection!")
+    rudp_s.sock.close()
+    return
+
+
+if __name__ == '__main__':
+    downloadmanager()
